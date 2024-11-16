3e0963311c5c0c59d819a85249f9c5b61e4b2282
Cooki-Studios <59518753+Cooki-Studios@users.noreply.github.com>
2024-11-16 19:51:44 +1300

    Fixed some stuff
    Don't worry there will be actual gameplay soon (v0.0.1-alpha)

---
+++ G1.gd
@@ -0,0 +1,7 @@
+extends TextureRect
+var shader_material : ShaderMaterial
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	shader_material = material as ShaderMaterial
+	shader_material.set_shader_parameter("scroll_speed", 3.0)
